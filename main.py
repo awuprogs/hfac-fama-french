7805881a2a3b318dd04338d47de61effa7e80b6f
Cecilia Zhou <zilingzhou@college.harvard.edu>
2015-10-27 10:25:43 -0400

    updated transaction object

--- main.py
+++ main.py
@@ -1,28 +1,31 @@
 import numpy as np
+import transaction
+import csv
+from itertools import islice
 
 if __name__ == '__main__':
     # read in transactions file and construct list of transactions
-    f = open('transactions.csv', 'wb')
     transactions = []
-    for line in f:
-        transactions.append(Transaction(line))
+    f = open('transactions.csv', 'rb')
+    for line in islice(f, 1, None):
+        transactions.append(transaction.Transaction(line))
 
-    # construct portfolios
-    portfolios = []
-    for t in transactions:
-        if len(portfolios = 0):
-            portfolios.append(Portfolio(t))
-        else:
-            portfolios.append(Portfolio(t, portfolios[-1]))
+    # # construct portfolios
+    # portfolios = []
+    # for t in transactions:
+    #     if len(portfolios = 0):
+    #         portfolios.append(Portfolio(t))
+    #     else:
+    #         portfolios.append(Portfolio(t, portfolios[-1]))
 
-    # TODO: figure out format for dates and way to get all trading days
-    # get portfolio values
-    idx = 0
-    values = []
-    for date in dates:
-        if portfolios[idx + 1].start_date <= date:
-            idx += 1
-        values.append(portfolios[idx].calculateValue(date))
+    # # TODO: figure out format for dates and way to get all trading days
+    # # get portfolio values
+    # idx = 0
+    # values = []
+    # for date in dates:
+    #     if portfolios[idx + 1].start_date <= date:
+    #         idx += 1
+    #     values.append(portfolios[idx].calculateValue(date))
 
     # TODO: calculate returns and/or output them
 
